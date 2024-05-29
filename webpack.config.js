d8bbd884e92b19d0b757b99156efdd5c35320bda
GabrielaZanetti <gabiisctt@gmail.com>
2024-05-29 16:12:28 -0300

    chore: inclusao do template do react app typescript

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/main/index.tsx',
+    output: {
+        path: path.join(__dirname, 'public/js'),
+        publicPath: '/public/js',
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.scss'],
+        alias: {
+            '@': path.join(__dirname, 'src')
+        }
+    },
+    module: {
+        rules: [{
+            test: /\.ts(x?)$/,
+            loader: 'ts-loader',
+            exclude: /node_modules/
+        }, {
+            test: /\.s[ac]ss$/i,
+            use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+            ],
+        }]
+    },
+    devServer: {
+        static: './public',
+        devMiddleware: {
+            writeToDisk: true,
+        },
+        historyApiFallback: true,
+    },
+    externals: {
+        react: 'React',
+        'react-dom': 'ReactDOM'
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ]
+}
